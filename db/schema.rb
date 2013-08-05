3fb160460dc5fe5c7964c1ba62d0764a11faa2b6
Ronny Polloqueri <ronny.polloqueri@gmail.com>
2013-08-05 13:59:10 -0500

    Agregado el login, falta, adecuar que partes deben requerir login, por ahora se ha aplicado a todo la aplicación

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130729234658) do
+ActiveRecord::Schema.define(version: 20130801172825) do
 
   create_table "marcas", force: true do |t|
     t.string   "descripcion"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20130729234658) do
 
   create_table "tipos", force: true do |t|
     t.string   "descripcion"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
